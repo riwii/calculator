c6d45ff29dd12f4da9ef0bc3f1f28f6489f3c36c
RONALDO RODRIGUEZ DE LIMA <165290429+MOKEXD@users.noreply.github.com>
2026-02-26 11:42:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
 saldo=1000
-print("Banco Corresponsal")
-
-operaciones = int(input("¿Cuantas operaciones desea realizar?: "))
-
-for i in range(operaciones):
-    selection= int(input(f"Seleccione la operacion deseada:\n1- Consultar saldo.\n2- Retirar.\n3- Depositar. \nOperacion #{i + 1}: "))
