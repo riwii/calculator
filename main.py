60e830a3dbec53ea283a1829a49b90c62db16a7e
RONALDO RODRIGUEZ DE LIMA <165290429+MOKEXD@users.noreply.github.com>
2026-02-26 08:31:14 -0500

    Refactor withdrawal logic for better validation

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-elif selection == "2":
-    while True:
-        monto = float(input("Ingrese el monto a retirar: ")) 
-
-        while monto <0: 
-            print ("monto invalido: ")
-            monto = float(input ("ingrese monto a retirar: "))
-
-        if monto > saldo_inicial: 
-                print ("saldo insuficiente")
 elif selection == 2:
-     saldo_inicial <= 0:
-     print("retiro exitoso")
-     print(f"nuevo saldo: {saldo_inicial - monto} " )
+        while True:
+            monto = float(input("Ingrese el monto a retirar: "))
+
+            if monto < 0:
+                print("Monto inválido")
+            elif monto > saldo:
+                print("Saldo insuficiente")
+            else:
+                saldo -= monto
+                print("Retiro exitoso")
+                print(f"Nuevo saldo: {saldo}")
+                break
