4b7be8c53ae247c236cd620eee803b06f06a861b
moises <ghmoises97@gmail.com>
2026-02-26 07:50:48 -0500

    cambio2

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-elif opcion == "2":
+elif selection == "2":
     while True:
-        monto = float(input("Ingrese el monto a retirar: "))
+        monto = float(input("Ingrese el monto a retirar: ")) 
 
+        while monto <0: 
+            print ("monto invalido: ")
+            monto = float(input ("ingrese monto a retirar: "))
+
+        if monto > saldo_inicial: 
+                print ("saldo insuficiente")
+
+        
+    
