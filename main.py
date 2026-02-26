274858f5a398038374fa37f0e1b0d8399f984c08
Camille Marrugo <marrugocamille@gmail.com>
2026-02-26 09:40:36 -0500

    Update main(ope).py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ print("Banco Corresponsal")
 operaciones = int(input("¿Cuantas operaciones desea realizar?: "))
 
 for i in range(operaciones):
-    selection= int(input("Seleccione la operacion deseada:\n1- Consultar saldo.\n2- Retirar.\n3- Depositar. "))
+    selection= int(input("Seleccione la operacion deseada:\n1- Consultar saldo.\n2- Retirar.\n3- Depositar. \n Operación : {i + 1}"))
