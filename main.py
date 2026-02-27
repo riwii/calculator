ff583f320585c299b29b69c68efe4a9d3530c565
RONALDO RODRIGUEZ DE LIMA <165290429+MOKEXD@users.noreply.github.com>
2026-02-27 06:21:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
- if selection == 3:
-        deposito = int(input("¿cuanto desea depositar?: "))
-        while (deposito <= 0):
-            print("Por favor ingrese un deposito válido")
-            deposito = int(input("¿cuanto desea depositar?: "))
-        saldo+=deposito
-        print("Su nuevo saldo es: ", saldo)
+elif selection == 3:
+        deposito = int(input("¿Cuánto desea depositar?: "))
+        while deposito <= 0:
+            print("Por favor ingrese un depósito válido")
+            deposito = int(input("¿Cuánto desea depositar?: "))
+
+        saldo += deposito
+        print("Su nuevo saldo es:", saldo)
+
+    else:
+        print("Opción no válida")
