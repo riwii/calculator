8adac7b7576665f326579f29458e739126e26131
YamitGC <yamitgc08@gmail.com>
2026-02-27 07:29:43 -0500

    opcion 2

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ for i in range(operaciones):
     selection= int(input(f"Seleccione la operacion deseada:\n1- Consultar saldo.\n2- Retirar.\n3- Depositar. \nOperacion #{i + 1}: "))
     if selection == 1:
         print (f"el saldo actual: {saldo}")
+    elif selection == 2:
+        while True:
+            monto = int(input("Ingrese el monto a retirar: "))
+
+            if monto <= 0:
+                print("Monto inválido")
+            elif monto > saldo:
+                print("Saldo insuficiente")
+            else:
+                saldo -= monto
+                print("Retiro exitoso")
+                print(f"Nuevo saldo: {saldo}")
+                break
